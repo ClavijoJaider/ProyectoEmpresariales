d9bb56f1177d98c34a8c4bf38f7e4afba2b6dec3
jaiderclavijo <jaiderclavijo@localhost>
2026-02-23 22:34:50 -0500

    Irendimiento y tarjeta de credito

--- src/main/java/model/TarjetaCredito.java
+++ src/main/java/model/TarjetaCredito.java
@@ -8,6 +8,18 @@ package model;
  *
  * @author jaiderclavijo
  */
+@Data
 public class TarjetaCredito {
+    
+    private double cupo;
+    private double cuotaManejo;
+    private String numeroTarjeta;
+    
+    public TarjetaCredito(double cupo, double cuotaManejo, String numeroTarjeta)
+    {
+        this.cupo = cupo;
+        this.cuotaManejo = cuotaManejo;
+        this.numeroTarjeta = numeroTarjeta;
+    }
     
 }
