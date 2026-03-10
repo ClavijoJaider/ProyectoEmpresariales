a21a9fd266cd6ca26df9258025ebe59440df1fba
jaiderclavijo <jaiderclavijo@localhost>
2026-03-10 14:18:39 -0500

    Avance del patron observer para implementar en GUI

---
+++ src/main/java/gui/ICambiable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+/**
+ *
+ * @author jaiderclavijo
+ */
+public interface ICambiable 
+{
+    public void cambio();
+}
